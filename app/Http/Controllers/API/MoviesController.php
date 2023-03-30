2300ebbee4506a47de30f8ba014fa83c61a86a0e
Joe <joe.luis@construsitebrasil.com.br>
2023-03-30 11:23:01 -0300

    Arrumando getLastPopularMovies para puxar os dados solicitados

--- app/Http/Controllers/API/MoviesController.php
+++ app/Http/Controllers/API/MoviesController.php
@@ -12,10 +12,22 @@ class MoviesController extends Controller
         $externalAPI = "https://api.themoviedb.org/3/movie/popular?api_key=b4bce14b774116c09c4f3805ad0f3dc0&language=pt-BR&page=1";
         $response = file_get_contents($externalAPI);
         $lastPopularMovies = json_decode($response);
+        foreach ($lastPopularMovies->results as $movie) {
+            $moviesArray = [
+                'nome' => $movie->original_title,
+                'generos' => $movie->genre_ids,
+                'adulto' => $movie->adult,
+                'pontuacao' => $movie->vote_average,
+                'poster' => $movie->poster_path,
+                'idioma_original' => $movie->original_language,
+                'sinopse' => $movie->overview
+            ];
+            
+            $movies[] = $moviesArray;
+        }
 
-        $collection = collect($lastPopularMovies->results);
-        $primeirosCinco = $collection->take($number);
-
-        return response()->json($primeirosCinco);
+        $collection = collect($movies);
+        $moviesByCount = $collection->take($number);
+        return response()->json($moviesByCount, 200);
     }
 }
